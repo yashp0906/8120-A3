b6f45af20c22db307cfa216ac83357632b29faee
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:06:01 -0400

    Implement test in calculateCanvasDiagonal for when large values

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -4,5 +4,8 @@ describe('calculateCanvasDiagonal', () => {
   test('should return the correct diagonal when width is 0', () => {
     expect(calculateCanvasDiagonal(0, 5)).toBe(5);
   });
+  test("Diagonal with large values", () => {
+    expect(calculateCanvasDiagonal(3000, 4000)).toBeCloseTo(5000);
+  });
 
 });
