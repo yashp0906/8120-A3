0e9d28895039ad491c58e5c593349dad383f6000
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:31:14 -0400

    Implement test in estimatePaintingTime for decimal speed

--- src/estimatePaintingTime/estimatePaintingTime.test.ts
+++ src/estimatePaintingTime/estimatePaintingTime.test.ts
@@ -4,5 +4,8 @@ describe('estimatePaintingTime', () => {
   test("estimates time to paint 100 sq ft at 20 sq ft/hr", () => {
     expect(estimatePaintingTime(100, 20)).toBe(5);
   });
+  test("estimates time with decimal speed", () => {
+    expect(estimatePaintingTime(30, 7.5)).toBe(4);
+  });
 });
 
