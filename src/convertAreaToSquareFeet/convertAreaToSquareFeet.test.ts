c200511ee3df95852bc714fd613f5d9953061694
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:28:33 -0400

    Implement test in convertAreaToSquareFeet for decimal number

--- src/convertAreaToSquareFeet/convertAreaToSquareFeet.test.ts
+++ src/convertAreaToSquareFeet/convertAreaToSquareFeet.test.ts
@@ -4,4 +4,7 @@ describe('convertAreaToSquareFeet', () => {
   test("converts 1 sq meter to sq feet", () => {
     expect(convertAreaToSquareFeet(1)).toBeCloseTo(10.7639, 4);
   });
+  test("converts decimal area", () => {
+    expect(convertAreaToSquareFeet(2.5)).toBeCloseTo(26.90975, 5);
+  });
 });
