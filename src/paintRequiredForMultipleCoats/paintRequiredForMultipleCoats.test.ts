7220efb40847a7457dff691e6ca53add2c35da46
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:38:23 -0400

    Implement test in paintRequiredForMultipleCoats for decimal area and covrage

--- src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.test.ts
+++ src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.test.ts
@@ -4,4 +4,7 @@ describe('paintRequiredForMultipleCoats', () => {
   test("100 sq ft, 10 sq ft/liter, 2 coats", () => {
     expect(paintRequiredForMultipleCoats(100, 10, 2)).toBe(20);
   });
+  test("decimal area and coverage", () => {
+    expect(paintRequiredForMultipleCoats(45.5, 9.1, 2)).toBeCloseTo(10, 5);
+  });
 });
