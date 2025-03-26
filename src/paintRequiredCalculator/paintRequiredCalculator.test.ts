6a5db30d9e11d19b068fdf98eb6924728e93a036
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:33:52 -0400

    Implement test in paintRequiredCalculator for fractional paint required

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -4,4 +4,7 @@ describe("paint required calculator", () => {
   test("calculates paint required for 100 sq ft with 10 sq ft/litre", () => {
     expect(paintRequiredCalculator(100, 10)).toBe(10);
   });
+  test("returns fractional paint required", () => {
+    expect(paintRequiredCalculator(30, 8)).toBeCloseTo(3.75, 2);
+  });
 });
