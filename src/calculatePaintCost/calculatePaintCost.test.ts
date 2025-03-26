f4dd8a8519a9286403a8480bcf9ef362f7004a78
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:21:06 -0400

    Implement test in calculatePaintCost for decimal unit

--- src/calculatePaintCost/calculatePaintCost.test.ts
+++ src/calculatePaintCost/calculatePaintCost.test.ts
@@ -4,4 +4,7 @@ describe('calculatePaintCost', () => {
   test("calculates paint cost for 100 sq units at $2 per unit", () => {
     expect(calculatePaintCost(100, 2)).toBe(200);
   });
+  test("decimal cost per unit", () => {
+    expect(calculatePaintCost(10, 2.5)).toBe(25);
+  });
 });
