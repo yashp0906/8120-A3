d497574f6276bb300d4a5b77acf2fddcbb272d27
yashp0906 <patelyashj9698@gmail.com>
2025-03-26 15:24:40 -0400

    Implement test in calculateTotalCost when paint cost and labor cost are 0

--- src/calculateTotalCost/calculateTotalCost.test.ts
+++ src/calculateTotalCost/calculateTotalCost.test.ts
@@ -4,4 +4,7 @@ describe('calculateTotalCost', () => {
   test("adds paint cost and labor cost", () => {
     expect(calculateTotalCost(100, 200)).toBe(300);
   });
+  test("both zero", () => {
+    expect(calculateTotalCost(0, 0)).toBe(0);
+  });
 });
